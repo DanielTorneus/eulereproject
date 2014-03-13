003cad8919a29c7d7db40267508881942cc43a95
Rasmus Larsson <rasmus@rasmuslarsson.se>
2014-03-13 22:31:27 +0100

    Problem 5

---
+++ problem_005_amusable.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3.3
+
+# Authors: AmusableLemur
+# Complexity: O(1)
+# Output: 232792560
+
+# 20 = 2 * 2 * 5
+# 19 = 19
+# 18 = 2 * 3 * 3
+# 17 = 17
+# 16 = 2 * 2 * 2 * 2
+# 15 = 3 * 5
+# 14 = 2 * 7
+# 13 = 13
+# 12 = 2 * 2 * 3
+# 11 = 11
+# Numbers below included above
+
+print(2 * 2 * 2 * 2 * 3 * 3 * 5 * 7 * 11 * 13 * 17 * 19)
